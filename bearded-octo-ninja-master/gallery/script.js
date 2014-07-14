2cd70fad86a3b904d415976ca44b26bb21d8bef9
Matt Jones <mattpauljones@gmail.com>
2014-07-14 12:39:19 -0500

    updated script

--- bearded-octo-ninja-master/gallery/script.js
+++ bearded-octo-ninja-master/gallery/script.js
@@ -1,1 +1,28 @@
-// Add your code here
+// // Add your code here
+// $(".filter-button").on("click", function(e){
+// 	e.preventDefault();
+
+// 	//trying to actually get the images to toggle
+// 	$(".this").toggle();
+// 	$(this).toggleClass("active");
+	
+// });
+
+$("#monkey").on("click", function(){
+	/*$(".monkey").hide();*/
+	$(".monkey").toggle(); /*show and hide*/
+	$(this).toggleClass("active"); /*makes button gray for monkeys*/
+});
+
+$("#dog").on("click", function(){
+	/*$(".monkey").hide();*/
+	$(".dog").toggle(); /*show and hide*/
+	$(this).toggleClass("active"); /*makes button gray for dog*/
+});
+
+$("#cat").on("click", function(){
+	/*$(".monkey").hide();*/
+	$(".cat").toggle(); /*show and hide*/
+	$(this).toggleClass("active"); /*makes button gray for cats*/
+});
+
